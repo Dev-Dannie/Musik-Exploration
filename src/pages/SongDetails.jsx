87dcb1959e76dfd3d588f41bc4779ba998efe6f3
Daniel Johnson <dj125466@gmail.com>
2022-12-10 04:12:46 +0100

    added related songs component

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -9,17 +9,26 @@ const SongDetails = () => {
 const dispatch = useDispatch();
 const {songid} = useParams();
 const {activeSong, isPlaying} = useSelector((state) => state.player);
-const {data: songData, isFetching: isFetchingSongDetails} =
+const {data: songData, isFetching: isFetchingSongDetails, error} =
 useGetSongDetailsQuery({songid});
 const {data, isFetching: isFetchingRelatedSongs} =
 useGetSongRelatedQuery({songid});
+
+const handlePauseClick = () => {
+   dispatch(playPause(false));
+ };
+ 
+ const handlePlayClick = (song, i) => {
+   dispatch(setActiveSong({song, data, i}))
+   dispatch(playPause(true));
+ };
 
 if (isFetchingRelatedSongs || isFetchingSongDetails) return
 <Loader title='Searching song details'/>;
 
 if (error) return <Error />
    return (
-     <div className="flex flex-col">
+     <div className="flex flex-col">  
         <DetailsHeader artistId='' songData={songData}/>
         <div className="mb-10">
             <h2 className="text-white text-3xl font-bold">Lyrics:</h2>
@@ -35,6 +44,8 @@ if (error) return <Error />
         data={data} 
         isPlaying={isPlaying}
         activeSong={activeSong}/>
+        handlePauseClick={handlePauseClick}
+        handlePlayClick={handlePlayClick}
      </div>
    )
 }
