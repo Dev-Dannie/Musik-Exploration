9a980b329ea7774835bf1ba3212a2bf2522d0820
Daniel Johnson <114375388+Dev-Dannie@users.noreply.github.com>
2023-01-02 22:27:11 +0100

    Update AroundYou.jsx

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -13,7 +13,7 @@ const CountryTracks = () => {
     const {data, isFetching, error} = useGetSongsByCountryQuery(country);    
 
     useEffect(() => {
-        axios.get('https://geo.ipify.org/api/v2/country?apiKey=at_dIgJVdL8Nr1UMpA8xAuJfwqvUexLe')
+        axios.get('https://geo.ipify.org/api/v2/country?apiKey=${import.meta.env.VITE_GEO_API_KEY}')
         .then((res) => setCountry(res?.data?.location.country))
         .catch((err) => console.log(err))
         .finally(() => setLoading(false));
